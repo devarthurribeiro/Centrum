663fb1652a72df384ad33cbc000d79d4b66adf60
Arthur Ribeiro <devarthurribeiro@gmail.com>
2020-03-27 12:53:45 -0300

    ✨add card component

---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card bg-secondary">
+    <h1 v-if="title" class="title">{{title}}</h1>
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: ['title']
+}
+</script>
+
+<style>
+.card {
+  width: 300px;
+  height: auto;
+  padding: 1em;
+  border-radius: 8px;
+}
+
+.card .title {
+  font-size: 24px;
+  margin-bottom: 16px;
+}
+</style>
